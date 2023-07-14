5b542a100a36337d317cbc23f08c70d7aa5a831a
Lucas Assis <46928741+lucasbdeassis@users.noreply.github.com>
2023-07-14 11:06:12 -0300

    ajustes para o mvp

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # mvp-desenvolvimento-fullstack-basico-backend
+
+Projeto desenvolvido durante o curso de Desenvolvimento Fullstack Básico da Pós Graduação em Engenhaia de Software da PUC Rio.
+
+## Como executar o projeto
+
+Recomenda-se a utilização do ambiente virtual do python para a execução do projeto. Para isso, siga os passos abaixo:
+
+1. Instale o virtualenv com o comando `python -m venv .venv`
+
+2. Ative o ambiente virtual com o comando `source .venv\Scripts\Activate.ps1`
+
+3. Instale as dependências do projeto com o comando `pip install -r requirements.txt`
+
+4. Execute o projeto com o comando `flask run`
+
+A aplicação estará disponível no endereço `http://localhost:5000`
--- src/api/transaction_controller.py
+++ src/api/transaction_controller.py
@@ -19,12 +19,12 @@ add_schema([TransactionCreateSchema, TransactionSchema, TransactionUpdateSchema]
 
 
 @transaction_controller.route("/transactions", methods=["POST"])
-def create():
+def create_transaction():
     """
     ---
     post:
-      summary: Adiciona uma nova transação
-      description: Adiciona uma nova transação
+      summary: Adds a new transaction
+      description: Adds a new transaction
       requestBody:
         required: true
         content:
@@ -32,7 +32,7 @@ def create():
             schema: TransactionCreateSchema
       responses:
         200:
-          description: Retorna a transação criada
+          description: the created transaction
           content:
             application/json:
               schema: TransactionCreateSchema
@@ -49,12 +49,12 @@ def create():
 
 
 @transaction_controller.route("/transactions/<id>", methods=["GET"])
-def get(id):
+def get_transaction(id):
     """
     ---
     get:
-      summary: Retorna uma transação por id
-      description: Retorna uma transação por id
+      summary: Returns a transaction
+      description: Returns a transaction
       parameters:
         - name: id
           in: path
@@ -63,7 +63,7 @@ def get(id):
             type : string
       responses:
         200:
-          description: Retorna o tenant criado
+          description: a transaction
           content:
             application/json:
               schema: TransactionSchema
@@ -80,11 +80,11 @@ def list_transaction():
     """
     ---
     get:
-      summary: Retorna uma lista de transações
-      description: Retorna uma lista de transações
+      summary: Returns a list of transactions
+      description: Returns a list of transactions
       responses:
         200:
-          description: Retorna o tenant criado
+          description: list of transactions
           content:
             application/json:
               schema: TransactionSchema
@@ -97,12 +97,12 @@ def list_transaction():
 
 
 @transaction_controller.route("/transactions/<id>", methods=["DELETE"])
-def delete(id):
+def delete_transaction(id):
     """
     ---
     delete:
-      summary: Remove uma transação
-      description: Remove uma transação
+      summary: Removes a transaction
+      description: Removes a transaction
       parameters:
         - name: id
           in: path
@@ -111,7 +111,7 @@ def delete(id):
             type : string
       responses:
         200:
-          description: Retorna o tenant criado
+          description: success message
           content:
             application/json:
               schema: TransactionSchema
@@ -121,88 +121,3 @@ def delete(id):
         service = TransactionService(repository)
         service.delete_transaction(id)
         return {"message": "Transaction deleted successfully"}, 200
-
-
-@transaction_controller.route("/transactions/<id>/tags", methods=["POST"])
-def add_tags(id):
-    """
-    ---
-    post:
-      summary: Adiciona uma nova tag
-      description: Adiciona uma nova tag
-      parameters:
-        - name: id
-          in: path
-          required: true
-          schema:
-            type : string
-      requestBody:
-        required: true
-        content:
-          application/json:
-            schema:
-              type: array
-              items:
-                type: string
-      responses:
-        200:
-          description: Retorna a transação editada
-          content:
-            application/json:
-              schema: TransactionCreateSchema
-    """
-    tags = request.json
-    if not isinstance(tags, list):
-        return {"error": "Tags must be a list"}, 400
-    with db_connection() as session:
-        repository = TransactionRepository(session)
-        service = TransactionService(repository)
-        transaction = service.add_tags(id, tags)
-        return transaction.dict(), 200
-
-
-@transaction_controller.route("/transactions/<id>/tags", methods=["DELETE"])
-def remove_tags(id):
-    """
-    ---
-    post:
-      summary: Remove uma tag
-      description: Remove uma tag
-      parameters:
-        - name: id
-          in: path
-          required: true
-          schema:
-            type : string
-      requestBody:
-        required: true
-        content:
-          application/json:
-            schema:
-              type: array
-              items:
-                type: string
-      responses:
-        200:
-          description: Retorna a transação editada
-          content:
-            application/json:
-              schema: TransactionCreateSchema
-    """
-    tags = request.json
-    if not isinstance(tags, list):
-        return {"error": "Tags must be a list"}, 400
-    with db_connection() as session:
-        repository = TransactionRepository(session)
-        service = TransactionService(repository)
-        transaction = service.remove_tags(id, tags)
-        return transaction.dict(), 200
-
-
-# @transaction_controller.route("/transaction/tags", methods=["GET"])
-# def list_trasactions_by_tag():
-#     with db_connection() as session:
-#         repository = TransactionRepository(session)
-#         service = TransactionService(repository)
-#         transactions = service.list_by_tag(request.args["tag"])
-#         return [transaction.dict() for transaction in transactions], 200
